a183a47d55a8500543effa47503aa51beeada150
Chris tomsaskavic <christomaskovic9@gmail.com>
2026-02-05 17:14:46 -0700

    Add GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/valentinesMiniGame/',
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> c4f230bcd29b8c7d22965f2deab62dedfd324f68
