6f176a9577e2b43f2d87f20ef8264b6234d63fa2
Chris tomsaskavic <christomaskovic9@gmail.com>
2026-02-05 17:29:36 -0700

    Resolve merge conflicts in tsconfig, postcss, tailwind, package-lock

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> c4f230bcd29b8c7d22965f2deab62dedfd324f68
